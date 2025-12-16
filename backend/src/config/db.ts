8436453f3cdac9241c15c4c6ea7d57b7f0f61cf6
Akzhol <akzholkozhatai@gmail.com>
2025-12-16 20:56:25 +0500

    Испралвние подключение с МонгоДБ

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/chat', {
+    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/realtime_chat", {
       autoIndex: true,
     });
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
